85bda2c04d6604a1322eb7c6d2f89dcd31f24697
Nhung Tran <nhungtrannn@gmail.com>
2019-03-05 17:11:45 -0600

    first

---
+++ exercise-2.py
@@ -0,0 +1,17 @@
+# exercise-02 Length of Phrase
+
+# Write the code that:
+# 1. Prompts the user to enter a phrase:
+#      Please enter a word or phrase: 
+# 2. Print the following message:
+#      - What you entered is xx characters long
+# 3. Return to step 1, unless the word 'quit' was entered.
+
+
+word = input('Please enter a word or phrase:')
+while word.lower() != 'quit': 
+    print(f'What you entered is {len(word)} characters long.')
+    word = input('Please enter a word or phrase:')
+    
+
+ 
